6942dbc6dab509da99e65427c7746ada487fecb2
Gold <goldenlandscape731@gmail.com>
2022-10-28 09:36:33 -0400

    Added Koala enable/disable + added Steam cache clear button

--- GLRToggle/Form1.Designer.cs
+++ GLRToggle/Form1.Designer.cs
@@ -32,12 +32,20 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(60, 148);
+            this.button1.Location = new System.Drawing.Point(60, 209);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(263, 67);
             this.button1.TabIndex = 0;
@@ -47,7 +55,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(60, 25);
+            this.button2.Location = new System.Drawing.Point(60, 83);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(263, 67);
             this.button2.TabIndex = 1;
@@ -58,18 +66,94 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(173, 95);
+            this.pictureBox1.Location = new System.Drawing.Point(173, 153);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(36, 50);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(211, 294);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(263, 35);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Clear Cached Steam Package Data";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(155, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 37);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "GLR";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Black", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(449, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 37);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Koala";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(479, 153);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(36, 50);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(366, 83);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(263, 67);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Enable";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(366, 209);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(263, 67);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Disable";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 354);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(574, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "If both buttons are disabled your install is not detected. Restart the program or" +
+    " install the unlocker correctly.";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.ClientSize = new System.Drawing.Size(399, 240);
+            this.ClientSize = new System.Drawing.Size(688, 378);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -77,7 +161,9 @@
             this.Name = "Form1";
             this.Text = "GLR Enabler/Disabler";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -86,5 +172,12 @@
         private Button button1;
         private Button button2;
         private PictureBox pictureBox1;
+        private Button button3;
+        private Label label1;
+        private Label label2;
+        private PictureBox pictureBox2;
+        private Button button4;
+        private Button button5;
+        private Label label3;
     }
 }
--- GLRToggle/Form1.cs
+++ GLRToggle/Form1.cs
@@ -9,6 +9,9 @@ namespace GLRToggle
         {
             string path = @"C:\Program Files (x86)\Steam\User32.dll";
             string path2 = @"C:\Program Files (x86)\Steam\disabled\User32.dll";
+            string path3 = @"C:\Program Files (x86)\Steam\appcache\appinfo.vdf";
+            string path4 = @"C:\Program Files (x86)\Steam\version.dll";
+            string path5 = @"C:\Program Files (x86)\Steam\disabled\version.dll";
 
             if (!File.Exists(path))
             {
@@ -28,6 +31,38 @@ namespace GLRToggle
             if (File.Exists(path2))
             {
                 button2.Enabled = true;
+            }
+
+            if (File.Exists(path3) | File.Exists(path4))
+            {
+                button3.Enabled = true;
+            }
+
+            if (!File.Exists(path3) & !File.Exists(path4))
+            {
+                button3.Enabled = false;
+            }
+
+            // If no version.dll in root, disable the Disable button
+            if (!File.Exists(path4))
+            {
+                button5.Enabled = false;
+            }
+
+            // If no version.dll in disabled, disable the Enable button
+            if (!File.Exists(path5))
+            {
+                button4.Enabled = false;
+            }
+
+            if (File.Exists(path4))
+            {
+                button5.Enabled = true;
+            }
+
+            if (File.Exists(path5))
+            {
+                button4.Enabled = true;
             }
         }
 
@@ -98,6 +133,120 @@ namespace GLRToggle
                 File.Move(path, path2);
                 Console.WriteLine("{0} was moved to {1}.", path, path2);
                 string message3 = "GLR enabled.";
+                MessageBox.Show(message3);
+                FileCheck();
+
+                // See if the original exists now.
+                if (File.Exists(path))
+                {
+                    Console.WriteLine("The original file still exists, which is unexpected.");
+                }
+                else
+                {
+                    Console.WriteLine("The original file no longer exists, which is expected.");
+                }
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine("The process failed: {0}", exc.ToString());
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string path = @"C:\Program Files (x86)\Steam\appcache\appinfo.vdf";
+            string path2 = @"C:\Program Files (x86)\Steam\appcache\packageinfo.vdf";
+
+            DialogResult dialogResult = MessageBox.Show("Are you sure you want to clear the Steam Cache? (appinfo.vdf and packageinfo.vdf will be permanently deleted!)", "Confirm", MessageBoxButtons.YesNo);
+
+            if (dialogResult == DialogResult.Yes)
+            {
+                try
+                {
+                    if (File.Exists(path))
+                    {
+                        File.Delete(path);
+                    }
+
+                    if (File.Exists(path2))
+                    {
+                        File.Delete(path2);
+                    }
+
+                    string message4 = "Deletion successful.";
+                    MessageBox.Show(message4);
+                    FileCheck();
+                }
+                catch
+                {
+                    string message5 = "Deletion failed or errors occured.";
+                    MessageBox.Show(message5);
+                    FileCheck();
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            System.IO.Directory.CreateDirectory(@"C:\Program Files (x86)\Steam\disabled");
+
+            string path = @"C:\Program Files (x86)\Steam\disabled\version.dll";
+            string path2 = @"C:\Program Files (x86)\Steam\version.dll";
+
+            try
+            {
+                // Ensure that the target does not exist.
+                if (File.Exists(path2))
+                {
+                    string message2 = "File exists in Steam folder. Stopping enable as two copies of version.dll exist.";
+                    MessageBox.Show(message2);
+                    FileCheck();
+                }
+
+                // Move the file.
+                File.Move(path, path2);
+                Console.WriteLine("{0} was moved to {1}.", path, path2);
+                string message3 = "Koala enabled.";
+                MessageBox.Show(message3);
+                FileCheck();
+
+                // See if the original exists now.
+                if (File.Exists(path))
+                {
+                    Console.WriteLine("The original file still exists, which is unexpected.");
+                }
+                else
+                {
+                    Console.WriteLine("The original file no longer exists, which is expected.");
+                }
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine("The process failed: {0}", exc.ToString());
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            System.IO.Directory.CreateDirectory(@"C:\Program Files (x86)\Steam\disabled");
+
+            string path = @"C:\Program Files (x86)\Steam\version.dll";
+            string path2 = @"C:\Program Files (x86)\Steam\disabled\version.dll";
+
+            try
+            {
+                // Ensure that the target does not exist.
+                if (File.Exists(path2))
+                {
+                    string message2 = "File exists in disabled folder. Stopping disable as two copies of version.dll exist.";
+                    MessageBox.Show(message2);
+                    FileCheck();
+                }
+
+                // Move the file.
+                File.Move(path, path2);
+                Console.WriteLine("{0} was moved to {1}.", path, path2);
+                string message3 = "Koala disabled.";
                 MessageBox.Show(message3);
                 FileCheck();
 
